1d22c8bf114039a5fc362330a3abfcf2b693055e
Alan <63099371+alansanchez96@users.noreply.github.com>
2022-09-03 16:09:41 -0300

    Update footer.blade.php

--- resources/views/layouts/componentes/footer.blade.php
+++ resources/views/layouts/componentes/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900 w-full">
+<footer class="p-4 mt-10 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900 w-full">
     <div class="sm:flex sm:items-center sm:justify-center sm:gap-5">
         <p class="text-gray-200 dark:text-zinc-400">Puedes encontrar más en</p>
         <a href="https://github.com/alansanchez96?tab=repositories" target="_blank"><i class='bx bxl-github text-gray-200 text-3xl'></i></a>
